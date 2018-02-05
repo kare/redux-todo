b3e0c6ad444bbc3abef438e0124de589fb64902e
Kare Nuorteva <kare.nuorteva@me.com>
2018-02-05 12:51:12 +0200

    Add ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "semi": [2, "never"],
+    "react/jsx-filename-extension": [1, { "extensions": [".js"] }]
+  },
+  "globals": {
+    "document": true,
+  }
+};
